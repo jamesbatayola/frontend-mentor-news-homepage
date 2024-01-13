0ebe4b4dd04313953d8e8e73153ec3568df62358
Jake James Batayola <batayolajakejames@gmail.com>
2024-01-14 00:19:04 +0800

    modified script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ body.addEventListener("click", (event) => {
 	if (!event.target.closest("nav")) {
 		return;
 	}
-	if (event.target.closest("nav")) {
-		console.log("jake");
+	if (event.target.closest("nav") && event.target.matches(".nav-menu-icon")) {
+		console.log("hello");
 	}
 });
