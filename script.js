01cf99ccf7f8177472312eaadd62135532053b48
Jake James Batayola <batayolajakejames@gmail.com>
2024-01-14 01:07:26 +0800

    updated

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 const body = document.body;
+const dropDownBG = document.querySelector(".dropdown-inactive");
 
 body.addEventListener("click", (event) => {
+	console.dir(event.target);
+	let dropDown;
 	if (!event.target.closest("nav")) {
 		return;
 	}
 	if (event.target.closest("nav") && event.target.matches(".nav-menu-icon")) {
-		console.log("hello");
+		dropDown = event.target.closest(".nav-menu-icon");
+		dropDown.classList.toggle("active");
+		dropDownBG.classList.toggle("dropdown-active");
+	}
+	if (!event.target.closest("dropdown") && !event.target.matches("nav-menu-icon")) {
+		dropDownBG.classList.remove("dropdown-active");
+		dropDown.classList.remove("active");
 	}
 });
