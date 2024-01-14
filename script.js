acd917736ab2048a714f41930315b6068f934f88
Jake James Batayola <batayolajakejames@gmail.com>
2024-01-14 16:36:23 +0800

    modified html css js

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-const body = document.body;
-const dropDownBG = document.querySelector(".dropdown-inactive");
+const dropdownMenu = document.querySelector(".nav-menu-icon");
+const dropdownClose = document.querySelector(".dropdown-close-icon");
+const dropDownBG = document.querySelector(".dropdown-background");
 
-body.addEventListener("click", (event) => {
-	console.dir(event.target);
-	let dropDown;
-	if (!event.target.closest("nav")) {
-		return;
-	}
-	if (event.target.closest("nav") && event.target.matches(".nav-menu-icon")) {
-		dropDown = event.target.closest(".nav-menu-icon");
-		dropDown.classList.toggle("active");
-		dropDownBG.classList.toggle("dropdown-active");
-	}
-	if (!event.target.closest("dropdown") && !event.target.matches("nav-menu-icon")) {
-		dropDownBG.classList.remove("dropdown-active");
-		dropDown.classList.remove("active");
-	}
+dropdownMenu.addEventListener("click", () => {
+	dropDownBG.classList.toggle("active");
+	dropdownMenu.classList.toggle("active");
+});
+
+dropdownClose.addEventListener("click", () => {
+	dropDownBG.classList.toggle("active");
+	dropdownMenu.classList.toggle("active");
 });
