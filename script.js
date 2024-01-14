e28de459fa84dc2999ee4e0d6096facb2618517e
Jake James Batayola <batayolajakejames@gmail.com>
2024-01-14 18:08:04 +0800

    desktop mode update

--- script.js
+++ script.js
@@ -2,12 +2,29 @@ const dropdownMenu = document.querySelector(".nav-menu-icon");
 const dropdownClose = document.querySelector(".dropdown-close-icon");
 const dropDownBG = document.querySelector(".dropdown-background");
 
+// home menu icon eventListener
 dropdownMenu.addEventListener("click", () => {
 	dropDownBG.classList.toggle("active");
 	dropdownMenu.classList.toggle("active");
 });
 
+// close menu icon eventListener
 dropdownClose.addEventListener("click", () => {
 	dropDownBG.classList.toggle("active");
 	dropdownMenu.classList.toggle("active");
 });
+
+// changing the illustration src based on window width
+let illustration = document.querySelector(".main-illustration");
+
+function windowCheck() {
+	if (window.innerWidth <= 700) {
+		illustration.src = "/assets/images/image-web-3-mobile.jpg";
+	} else {
+		illustration.src = "/assets/images/image-web-3-desktop.jpg";
+	}
+}
+
+window.addEventListener("resize", windowCheck);
+
+windowCheck();
