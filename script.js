c6a523755d42c364d106ad545c9c56e3d542a91d
Jake James Batayola <batayolajakejames@gmail.com>
2024-01-14 21:31:21 +0800

    final

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ const dropDownBG = document.querySelector(".dropdown-background");
 
 // home menu icon eventListener
 dropdownMenu.addEventListener("click", () => {
-	dropDownBG.classList.toggle("active");
-	dropdownMenu.classList.toggle("active");
+	dropDownBG.classList.add("active");
+	dropdownMenu.classList.add("active");
 });
 
 // close menu icon eventListener
 dropdownClose.addEventListener("click", () => {
-	dropDownBG.classList.toggle("active");
-	dropdownMenu.classList.toggle("active");
+	dropDownBG.classList.remove("active");
+	dropdownMenu.classList.remove("active");
 });
 
 // changing the illustration src based on window width
@@ -20,6 +20,7 @@ let illustration = document.querySelector(".main-illustration");
 function windowCheck() {
 	if (window.innerWidth <= 700) {
 		illustration.src = "/assets/images/image-web-3-mobile.jpg";
+		dropDownBG.classList.remove("active");
 	} else {
 		illustration.src = "/assets/images/image-web-3-desktop.jpg";
 	}
